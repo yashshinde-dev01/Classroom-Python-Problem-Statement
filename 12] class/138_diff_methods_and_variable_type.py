9ec9b68e0e0bec4cc5c3db122026a164c8fc5bd1
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-04-21 13:06:45 +0530

    value changed

--- 12] class/138_diff_methods_and_variable_type.py
+++ 12] class/138_diff_methods_and_variable_type.py
@@ -1,6 +1,6 @@
 #to demonstrate diff. methods and variable types in class of python
 class first:
-    x=25                    #class variable
+    x=26                   #class variable
     def show(self):         #instance method
         print('This is instance method')
 
